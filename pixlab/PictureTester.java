cf558fea673139488da1f015303364d24a90aa20
kdoty34h <k3v1n.y34h@gmail.com>
2019-03-12 12:24:09 -0700

    help

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -58,7 +58,7 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
@@ -66,7 +66,7 @@ public class PictureTester
     //testGrayscale();
     //testFixUnderwater();
     testMirrorVertical();
-    //testMirrorTemple();
+    testMirrorTemple();
     //testMirrorArms();
     //testMirrorGull();
     //testMirrorDiagonal();
